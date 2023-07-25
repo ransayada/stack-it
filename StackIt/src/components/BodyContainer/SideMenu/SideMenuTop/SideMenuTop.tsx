9a112383aa526f9fb7b3da8e5a5904e5bbae4992
ransayada <ran.sayada@gmail.com>
2023-07-25 13:19:41 +0300

    wip

--- StackIt/src/components/BodyContainer/SideMenu/SideMenuTop/SideMenuTop.tsx
+++ StackIt/src/components/BodyContainer/SideMenu/SideMenuTop/SideMenuTop.tsx
@@ -1,12 +1,32 @@
 import { FC } from "react";
 import "./side-menu-top.scss";
+import { AiOutlineHome } from "react-icons/ai";
+import { BsArrowUpRightCircleFill } from "react-icons/bs";
 
 type SideMenuTopProps = {
   // user:any;
 };
 
 const SideMenuTop: FC<SideMenuTopProps> = () => {
-  return <div>SideMenuTop</div>;
+  return (
+    <div className="side-menu-top-container">
+      <p className="feeds">FEEDS</p>
+      <div>
+        <div className="home-btn-container">
+          <div className="home-icon">
+            <AiOutlineHome />
+          </div>
+          <h2>Home</h2>
+        </div>
+        <div className="popular-btn-container">
+          <div className="popular-icon">
+            <BsArrowUpRightCircleFill />
+          </div>
+          <h2>Popular</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SideMenuTop;
